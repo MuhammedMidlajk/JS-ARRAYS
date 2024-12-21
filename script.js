a0a95a5b47997f1936712ed2d1b2fb350c9c269d
MuhammedMidlajk <midlajmuhamd@gmial.com>
2024-12-21 10:10:06 +0530

    JS-ARRAYS

---
+++ script.js
@@ -0,0 +1,204 @@
+    
+    // with method
+
+
+// var cars= ["mercedes", "Volvo", "BMW"];
+// console.log(cars)
+
+
+//  to join 
+
+// var cars= ["MERCEDES", "Volvo", "BMW", "AUDI"];
+// console.log(cars.join("*"))
+
+
+//  index number
+
+// var cars= ["MERCEDES", "Volvo", "BMW", "AUDI"];
+// console.log(cars.at("3"))
+
+
+
+//  length check
+
+// var cars= ["MERCEDES", "Volvo", "BMW", "AUDI"];
+// console.log(cars.length)
+
+
+
+//  last remove
+
+// var cars= ["MERCEDES", "Volvo", "BMW", "AUDI"];
+// cars.pop();
+// console.log(cars)
+
+
+
+//  last add
+
+// var cars= ["MERCEDES", "Volvo", "BMW", "AUDI"];
+// cars.push("BENTLY");
+// console.log(cars)
+
+
+
+// //  first add
+
+// var cars= ["MERCEDES", "Volvo", "BMW", "AUDI"];
+// cars.unshift("porsche");
+// console.log(cars)
+
+
+//   remove first arrey
+
+//  var cars= ["MERCEDES", "Volvo", "BMW", "AUDI"];
+//  cars.shift();
+//  console.log(cars)
+
+
+        // without method
+
+
+// to replace 
+
+// let cars= ["MERCEDES", "VOLVO", "BMW", "AUDI"];
+// console.log(cars);
+// cars[1]="TOYOTA"
+// console.log(cars)
+
+
+//  replace first one 
+
+// let cars = ["MERCEDES", "VOLVO", "BMW", "AUDI"];
+
+// cars[0]="LAND ROVER ";
+// console.log(cars);
+
+
+
+// let cars = ["MERCEDES", "VOLVO", "BMW", "AUDI"];
+// var x = cars.length;
+
+// for (var i = 0; i <=x; i++) {
+
+//     console.log(
+//         cars[i] 
+//     );
+// }
+
+
+
+
+
+// to replace lastone 
+
+// const fruit=["apple","banana","mango"]
+
+// fruit[2]="kiwi"
+// console.log(fruit);
+
+
+
+// //  two value same one 
+
+// const fruit=["apple","banana","mango","kiwis"]
+
+// fruit[1]=fruit[2]
+// console.log(fruit);
+
+
+
+
+// to add last element
+
+// const fruit=["apple","banana","mango","kiwis","kkkk"]
+
+// fruit[fruit.length]="grape"
+// console.log(fruit);
+
+
+// add first element
+
+// const fruit=["apple","banana","mango","kiwi","STROBERRY"]
+
+// var a=fruit.length
+//   for(let i=a-1;i>=0;i--){
+//     fruit[i+1]=fruit[i]
+
+// }
+// fruit[0]="Litchi"
+// console.log(fruit);
+
+
+
+// delete first  element
+
+// const brand=["locast","levis","us polo","diesel","nike"]
+
+// var a=brand.length
+// for(let i=0;i<a;i++){
+
+//     brand[i]=brand[i+1]
+// }
+// brand.length--       
+// console.log(brand)
+  
+
+
+// // delete last  element
+
+// const brand=["locast","levis","us polo","diesel","nike"]
+
+// brand.length--       
+// console.log(brand)
+ 
+
+// delete center element
+
+// const number=[1,2,3,4,5,6,7,8,9,10]
+
+// var a=Math.floor(number.length/2)
+// for(let i=a;i<number.length-1;i++){
+
+//     number[i]=number[i+1]
+// }
+// number.length--       
+// console.log(number)
+  
+
+
+// delete center 2 element
+
+// const number=[1,2,3,4,5,6,7,8]
+
+// var a=Math.floor(number.length/2-1)
+
+// for(let i=a;i<number.length-1;i++){
+
+//     number[i]=number[i+2]
+// }
+// number.length-=2       
+// console.log(number)
+
+
+
+
+
+// const number=[1,2,3,4,5,6,7,8]      
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+ 
+
+
+// console.log(number)
+
+
+
+// const arr =[5,1,3,2,6];
+
+// function binary(x) {
+
+//     return x * 2;
+// }
+// const output = arr.map(binary);
+
+// console.log(output);
